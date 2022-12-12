a2e8fa37b51db6762948a340037f7f72dbe2c2ff
OlyaEf <efimovskikh@gmail.com>
2022-12-12 09:17:55 +0000

    add function commit

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ def print_hi(text):
     print(f'Hi, {text}')
 
 
+def commit(text):
+    print(f'{text}')
+
+
 if __name__ == '__main__':
     print_hi('это мой проект')
+    commit('коммит от Оли')
+
+
