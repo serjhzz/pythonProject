cac572f2b8ef67ba9566473635060cd21b2325c1
Veronika <veronika.apakova@mail.ru>
2022-12-12 13:05:04 +0300

    add new_commit

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def print_hi(text):
 def commit(text):
     print(f'{text}')
 
+def new_commit(test):
+    print(f"{test}")
+
 
 if __name__ == '__main__':
     print_hi('это мой проект')
     commit('Коммит от Оли')
+    new_commit("Коммит от Ники")
